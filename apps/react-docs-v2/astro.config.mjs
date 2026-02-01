74b0187578ee5b40bcc91a1933ab99074897c53d
duyph <ph.duy113@gmail.com>
2026-02-01 15:00:55 +0700

    core: adjust styling for mdx component

--- apps/react-docs-v2/astro.config.mjs
+++ apps/react-docs-v2/astro.config.mjs
@@ -7,9 +7,75 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [react(), mdx()],
+	integrations: [
+		react(),
+		mdx({
+			// add class name to output html tags from markdown for custom styling
+			// https://mdxjs.com/packages/remark-rehype/#handlers
+			remarkRehype: {
+				handlers: {
+					table(state, node) {
+						return {
+							type: 'element',
+							tagName: 'Table',
+							properties: {
+								className: ['mdx-table']
+							},
+							children: state.all(node)
+						};
+					},
+
+					inlineCode(_, node) {
+						return {
+							type: 'element',
+							tagName: 'code',
+							properties: {
+								className: ['mdx-inline-code']
+							},
+							children: [
+								{
+									type: 'text',
+									value: node.value
+								}
+							]
+						};
+					},
+					heading(state, node) {
+						const tagName = `h${node.depth}`;
+						return {
+							type: 'element',
+							tagName,
+							properties: {
+								className: ['mdx-heading']
+							},
+							children: state.all(node)
+						};
+					},
+					paragraph(state, node) {
+						return {
+							type: 'element',
+							tagName: 'p',
+							properties: {
+								className: ['mdx-paragraph']
+							},
+							children: state.all(node)
+						};
+					},
+					thematicBreak() {
+						return {
+							type: 'element',
+							tagName: 'hr',
+							properties: {
+								className: ['mdx-hr']
+							},
+							children: []
+						};
+					}
+				}
+			}
+		})
+	],
 
 	vite: {
 		plugins: [tailwindcss()],
