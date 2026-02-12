79fcf5e3b508d284590fb68af87ed68a990c48fe
duyph <ph.duy113@gmail.com>
2026-02-13 00:06:36 +0700

    [package][react-ui][refactor] improve Select

--- packages/react-ui/stories/FormField/MultipleSelect.stories.tsx
+++ packages/react-ui/stories/FormField/MultipleSelect.stories.tsx
@@ -1,5 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import MultipleSelect from '@components/MultipleSelect';
+import { useState } from 'react';
+import { expect, within, userEvent, screen, fn } from 'storybook/test';
+
+const mockedOnValueChange = fn();
+
+const items = [
+	{ label: 'React', value: 'react' },
+	{ label: 'Vue', value: 'vue' },
+	{ label: 'Angular', value: 'angular' },
+	{ label: 'Svelte', value: 'svelte' }
+];
+
+const clearButtonLabel = 'Clear value';
+const selectIndicatorLabel = 'select indicator';
 
 const meta: Meta<typeof MultipleSelect> = {
 	title: 'Components/FormField/MultipleSelect',
@@ -13,7 +27,17 @@ const meta: Meta<typeof MultipleSelect> = {
 		status: {
 			control: 'select',
 			options: ['default', 'error', 'success', 'warning']
-		}
+		},
+		clearable: { control: 'boolean' },
+		disabled: { control: 'boolean' },
+		supportingText: { control: 'text' }
+	},
+	args: {
+		items,
+		supportingText: 'Please select at least one item.'
+	},
+	beforeEach() {
+		mockedOnValueChange.mockClear();
 	}
 };
 
@@ -21,34 +45,334 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const items = [
-	{ label: 'React', value: 'react' },
-	{ label: 'Vue', value: 'vue' },
-	{ label: 'Angular', value: 'angular' },
-	{ label: 'Svelte', value: 'svelte' }
-];
-
 export const Default: Story = {
 	args: {
 		label: 'Frameworks',
 		placeholder: 'Select frameworks',
-		items
-	}
-};
-
-export const WithValues: Story = {
-	args: {
-		label: 'Frameworks',
-		value: ['react', 'vue'],
-		items
-	}
-};
-
-export const Error: Story = {
-	args: {
-		label: 'Frameworks',
-		status: 'error',
 		supportingText: 'Please select at least one framework.',
-		items
+		'data-testId': 'multiple-select-default'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if container exists', async () => {
+			expect(container).toBeInTheDocument();
+		});
+
+		await step('Check if label exists', async () => {
+			const labelElement = within(container).getByText(label);
+			expect(labelElement).toBeInTheDocument();
+		});
+
+		await step('Check if trigger exists', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeInTheDocument();
+		});
+
+		await step('Check if Select has placeholder', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			const placeholder = within(trigger).getByText(args.placeholder as string);
+			expect(placeholder).toBeInTheDocument();
+		});
+
+		await step('Check if Select has supporting text', async () => {
+			const supportingText = within(container).getByText(args.supportingText as string);
+			expect(supportingText).toBeInTheDocument();
+			expect(supportingText).toHaveAttribute('id');
+
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveAttribute('aria-describedby', supportingText.id);
+		});
+
+		await step('Check if indicator is displayed', async () => {
+			const indicator = within(container).getByLabelText(selectIndicatorLabel);
+			expect(indicator).toBeInTheDocument();
+		});
+
+		await step('Check if hidden select is rendered', async () => {
+			const hiddenSelect = within(container).getByLabelText(label, { selector: 'select' });
+			expect(hiddenSelect).toBeInTheDocument();
+		});
+	}
+};
+
+export const SelectItemFlow: Story = {
+	args: {
+		label: 'Frameworks',
+		'data-testId': 'multiple-select-select-item-flow'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Open menu', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			await userEvent.click(trigger);
+		});
+
+		const menuPopup = screen.getByRole('listbox', { name: label });
+		await step('Check if menu popup is displayed', async () => {
+			expect(menuPopup).toBeVisible();
+		});
+
+		await step('Check if menu popup consists all options from the item list', async () => {
+			items.forEach((item) => {
+				const option = within(menuPopup).getByRole('option', { name: item.label });
+				expect(option).toBeInTheDocument();
+			});
+		});
+
+		await step('Select multiple options', async () => {
+			const firstOption = within(menuPopup).getByRole('option', { name: items[0].label });
+			await userEvent.click(firstOption);
+			const secondOption = within(menuPopup).getByRole('option', { name: items[1].label });
+			await userEvent.click(secondOption);
+		});
+
+		await step('Check if trigger shows the selected values', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent(items[0].label);
+			expect(trigger).toHaveTextContent(items[1].label);
+		});
+
+		await step('Check if menu popup is still visible (multiple select behavior)', async () => {
+			expect(menuPopup).toBeVisible();
+		});
+
+		await step('Close menu popup', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			await userEvent.click(trigger);
+			expect(menuPopup).not.toBeVisible();
+		});
+	}
+};
+
+export const WithDefaultValue: Story = {
+	args: {
+		label: 'Frameworks',
+		'data-testId': 'multiple-select-with-default-value',
+		defaultValue: [items[0].value, items[1].value],
+		clearable: true
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if the trigger shows the default values', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent(items[0].label);
+			expect(trigger).toHaveTextContent(items[1].label);
+		});
+
+		await step('Check if the clear icon is showed', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			expect(clearButton).toBeVisible();
+		});
+	}
+};
+
+export const Clearable: Story = {
+	args: {
+		label: 'Frameworks',
+		clearable: true,
+		'data-testId': 'multiple-select-clearable'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if the clear icon is hidden when there is no selected value', async () => {
+			const clearButton = within(container).queryByRole('button', { name: clearButtonLabel });
+			expect(clearButton).not.toBeInTheDocument();
+		});
+
+		await step('Select items', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			await userEvent.click(trigger);
+			const menuPopup = screen.getByRole('listbox', { name: label });
+
+			const firstOption = within(menuPopup).getByRole('option', { name: items[0].label });
+			await userEvent.click(firstOption);
+			const secondOption = within(menuPopup).getByRole('option', { name: items[1].label });
+			await userEvent.click(secondOption);
+		});
+
+		await step('Check if close Icon is showed', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			expect(clearButton).toBeVisible();
+		});
+
+		await step('Clear the selected value', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			await userEvent.click(clearButton);
+		});
+
+		await step('Check if value is clear', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent('');
+		});
+
+		await step('Check if the clear button is hidden', async () => {
+			const clearButton = within(container).queryByRole('button', { name: clearButtonLabel });
+			expect(clearButton).not.toBeInTheDocument();
+		});
+	}
+};
+
+export const Controllable: Story = {
+	args: {
+		label: 'Frameworks',
+		clearable: true,
+		value: [items[0].value],
+		'data-testId': 'multiple-select-controllable',
+		onValueChange: mockedOnValueChange
+	},
+	render: (args) => {
+		const [value, setValue] = useState(args.value);
+
+		const handleChange = (newValue: string[], item: any) => {
+			setValue(newValue);
+			if (args.onValueChange) {
+				args.onValueChange(newValue, item);
+			}
+		};
+		return (
+			<div className="flex flex-col gap-2">
+				<MultipleSelect {...args} value={value} onValueChange={handleChange} />
+				<p className="mt-4" aria-label="selected-value">
+					Selected: {value?.join(', ')}
+				</p>
+			</div>
+		);
+	},
+	play: async ({ canvas, args, step }) => {
+		const { label = '', 'data-testId': testId = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if trigger exists', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeInTheDocument();
+		});
+
+		const trigger = within(container).getByRole('combobox', { name: label });
+		await step('Check if trigger has pre-selected value', async () => {
+			expect(trigger).toHaveTextContent(items[0].label);
+		});
+
+		await step('Select another item', async () => {
+			await userEvent.click(trigger);
+			const menuPopup = screen.getByRole('listbox', { name: label });
+
+			const secondOption = within(menuPopup).getByRole('option', { name: items[1].label });
+			await userEvent.click(secondOption);
+		});
+
+		await step('Check if onValueChange is called', async () => {
+			expect(mockedOnValueChange).toHaveBeenCalled();
+			expect(mockedOnValueChange).toHaveBeenCalledWith(
+				[items[0].value, items[1].value],
+				[items[0], items[1]]
+			);
+		});
+
+		await step('Check if external state is updated with new value', async () => {
+			const displayedValue = canvas.getByLabelText('selected-value');
+			expect(displayedValue).toHaveTextContent(`Selected: ${items[0].value}, ${items[1].value}`);
+		});
+
+		await step('Clear all values', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			await userEvent.click(clearButton);
+		});
+
+		await step('Check if value is clear', async () => {
+			const displayedValue = canvas.getByLabelText('selected-value');
+			expect(displayedValue).toHaveTextContent('Selected:');
+		});
+
+		await step('Check if onValueChange received empty array', async () => {
+			// Second call should be empty array
+			expect(mockedOnValueChange).toHaveBeenLastCalledWith([], []);
+		});
+	}
+};
+
+export const Status: Story = {
+	render: (args) => {
+		return (
+			<div className="flex flex-col gap-2">
+				<MultipleSelect
+					{...args}
+					status="error"
+					label="Error"
+					supportingText="Please select at least one framework."
+				/>
+				<MultipleSelect
+					{...args}
+					status="success"
+					label="Success"
+					supportingText="Please select at least one framework."
+				/>
+				<MultipleSelect
+					{...args}
+					status="warning"
+					label="Warning"
+					supportingText="Please select at least one framework."
+				/>
+			</div>
+		);
+	}
+};
+
+export const Size: Story = {
+	args: {},
+	render: (args) => {
+		return (
+			<div className="flex flex-col gap-2">
+				<MultipleSelect {...args} size="small" label="Small" />
+				<MultipleSelect {...args} size="medium" label="Medium" />
+			</div>
+		);
+	}
+};
+
+export const Disabled: Story = {
+	args: {
+		label: 'Frameworks',
+		disabled: true,
+		placeholder: 'Select Frameworks',
+		'data-testId': 'multiple-select-disabled'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { label = '', 'data-testId': testId = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if trigger is disabled', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeDisabled();
+		});
+	}
+};
+
+export const Required: Story = {
+	args: {
+		label: 'Frameworks',
+		required: true,
+		'data-testId': 'multiple-select-required'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { label = '', 'data-testId': testId = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if required indicator is visible', async () => {
+			const requiredSymbol = within(container).getByText('*');
+			expect(requiredSymbol).toBeVisible();
+		});
+
+		await step('Check if trigger is required', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeRequired();
+		});
 	}
 };
--- packages/react-ui/stories/FormField/SingleSelect.stories.tsx
+++ packages/react-ui/stories/FormField/SingleSelect.stories.tsx
@@ -1,5 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import SingleSelect from '@components/SingleSelect';
+import { useState } from 'react';
+import { expect, within, userEvent, screen, fn } from 'storybook/test';
+
+const mockedOnValueChange = fn();
+
+const items = [
+	{ label: 'React', value: 'react' },
+	{ label: 'Vue', value: 'vue' },
+	{ label: 'Angular', value: 'angular' },
+	{ label: 'Svelte', value: 'svelte' }
+];
+
+const clearButtonLabel = 'Clear value';
+
+const selectIndicatorLabel = 'select indicator';
 
 const meta: Meta<typeof SingleSelect> = {
 	title: 'Components/FormField/SingleSelect',
@@ -19,20 +34,17 @@ const meta: Meta<typeof SingleSelect> = {
 		supportingText: { control: 'text' }
 	},
 	args: {
-		supportingText: 'Please select a item.'
+		supportingText: 'Please select a item.',
+		items: items
+	},
+	beforeEach() {
+		mockedOnValueChange.mockClear();
 	}
 };
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-const items = [
-	{ label: 'React', value: 'react' },
-	{ label: 'Vue', value: 'vue' },
-	{ label: 'Angular', value: 'angular' },
-	{ label: 'Svelte', value: 'svelte' }
-];
 
 /**
  * The default single select with a label.
@@ -41,30 +53,244 @@ export const Default: Story = {
 	args: {
 		label: 'Framework',
 		placeholder: 'Select a framework',
-		items,
-		supportingText: 'Please select a framework.'
+		supportingText: 'Please select a framework.',
+		'data-testId': 'single-select-default'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if container exists', async () => {
+			expect(container).toBeInTheDocument();
+		});
+
+		await step('Check if label exists', async () => {
+			const labelElement = within(container).getByText(label);
+			expect(labelElement).toBeInTheDocument();
+		});
+
+		await step('Check if trigger exists', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeInTheDocument();
+		});
+
+		await step('Check if Select has placeholder', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent(args.placeholder as string);
+		});
+
+		await step('Check if Select has supporting text', async () => {
+			const supportingText = within(container).getByText(args.supportingText as string);
+			expect(supportingText).toBeInTheDocument();
+			expect(supportingText).toHaveAttribute('id');
+
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveAttribute('aria-describedby', supportingText.id);
+		});
+
+		await step('Check if indicator is displayed', async () => {
+			const indicator = within(container).getByLabelText(selectIndicatorLabel);
+			expect(indicator).toBeInTheDocument();
+		});
+
+		await step('Check if hidden select is rendered', async () => {
+			const hiddenSelect = within(container).getByLabelText(label, { selector: 'select' });
+			expect(hiddenSelect).toBeInTheDocument();
+		});
 	}
 };
-/**
- * Single select with a pre-selected value.
- */
-export const Controllable: Story = {
+
+export const SelectItemFlow: Story = {
 	args: {
 		label: 'Framework',
-		value: 'react',
-		items
+		'data-testId': 'single-select-select-item-flow'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Select Item', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			await userEvent.click(trigger);
+		});
+
+		const menuPopup = screen.getByRole('listbox', { name: label });
+		await step('Check if menu popup is displayed', async () => {
+			expect(menuPopup).toBeVisible();
+		});
+
+		await step('Check if menu popup consists all options from the item list', async () => {
+			items.forEach((item) => {
+				const option = within(menuPopup).getByRole('option', { name: item.label });
+				expect(option).toBeInTheDocument();
+			});
+		});
+
+		await step('user click on the first option', async () => {
+			const firstOption = within(menuPopup).getByRole('option', { name: items[0].label });
+			await userEvent.click(firstOption);
+		});
+
+		await step('Check if menu popup is hidden', async () => {
+			expect(menuPopup).not.toBeVisible();
+		});
+
+		await step('Check if trigger show the selected value', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent(items[0].label);
+		});
+	}
+};
+
+export const WithDefaultValue: Story = {
+	args: {
+		label: 'Framework',
+		'data-testId': 'single-select-with-default-value',
+		defaultValue: items[0].value,
+		clearable: true
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if the trigger show the default value', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent(items[0].label);
+		});
+
+		await step('Check if the clear icon is showed', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			expect(clearButton).toBeVisible();
+		});
+	}
+};
+
+export const Clearable: Story = {
+	args: {
+		label: 'Framework',
+		clearable: true,
+		'data-testId': 'single-select-clearable'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { 'data-testId': testId = '', label = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if the clear icon is hidden when there is no selected value', async () => {
+			const clearButton = within(container).queryByRole('button', { name: clearButtonLabel });
+			expect(clearButton).not.toBeInTheDocument();
+		});
+
+		await step('Select first item', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			await userEvent.click(trigger);
+			const menuPopup = screen.getByRole('listbox', { name: label });
+
+			const firstOption = within(menuPopup).getByRole('option', { name: items[0].label });
+			await userEvent.click(firstOption);
+		});
+
+		await step('Check if close Icon is showed', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			expect(clearButton).toBeVisible();
+		});
+
+		await step('Clear the selected value', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			await userEvent.click(clearButton);
+		});
+
+		await step('Check if value is clear', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toHaveTextContent('');
+		});
+
+		await step('Check if the clear button is hidden', async () => {
+			const clearButton = within(container).queryByRole('button', { name: clearButtonLabel });
+			expect(clearButton).not.toBeInTheDocument();
+		});
 	}
 };
 
 /**
- * Single select with error status.
+ * Single select with a pre-selected value.
  */
-export const Error: Story = {
+
+export const Controllable: Story = {
 	args: {
 		label: 'Framework',
-		status: 'error',
-		supportingText: 'Please select a framework.',
-		items
+		clearable: true,
+		value: items[0].value,
+		onValueChange: mockedOnValueChange,
+		'data-testId': 'single-select-controllable'
+	},
+	render: (args) => {
+		const [value, setValue] = useState(args.value);
+
+		const handleChange = (newValue: string, item: any) => {
+			setValue(newValue);
+			if (args.onValueChange) {
+				args.onValueChange(newValue, item);
+			}
+		};
+		return (
+			<div className="flex flex-col gap-2">
+				<SingleSelect {...args} value={value} onValueChange={handleChange} />
+				<p className="mt-4" aria-label="selected-value">
+					Selected: {value}
+				</p>
+			</div>
+		);
+	},
+	play: async ({ canvas, args, step }) => {
+		const { label = '', 'data-testId': testId = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if trigger exists', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeInTheDocument();
+		});
+
+		const trigger = within(container).getByRole('combobox', { name: label });
+		await step('Check if trigger has pre-selected value', async () => {
+			expect(trigger).toHaveTextContent(items[0].label);
+		});
+
+		await step('Select an item', async () => {
+			await userEvent.click(trigger);
+			const menuPopup = screen.getByRole('listbox', { name: label });
+
+			const firstOption = within(menuPopup).getByRole('option', { name: items[1].label });
+			await userEvent.click(firstOption);
+		});
+
+		await step('Check if onValueChange is called', async () => {
+			expect(mockedOnValueChange).toHaveBeenCalled();
+			expect(mockedOnValueChange).toHaveBeenCalledWith(items[1].value, items[1]);
+		});
+
+		await step('Check if external state is updated with new value', async () => {
+			const displayedValue = canvas.getByLabelText('selected-value');
+			expect(displayedValue).toHaveTextContent('Selected: ' + items[1].value);
+		});
+
+		await step('Clear selected value', async () => {
+			const clearButton = within(container).getByRole('button', { name: clearButtonLabel });
+			await userEvent.click(clearButton);
+		});
+
+		await step('Check if value is clear', async () => {
+			const displayedValue = canvas.getByLabelText('selected-value');
+			expect(displayedValue).toHaveTextContent('Selected:');
+		});
+
+		await step('Check if onValueChange received empty string argument', async () => {
+			expect(mockedOnValueChange).toHaveBeenNthCalledWith(2, '', undefined);
+		});
+
+		await step('Check if clear button is hidden', async () => {
+			const clearButton = within(container).queryByRole('button', { name: clearButtonLabel });
+			expect(clearButton).not.toBeInTheDocument();
+		});
 	}
 };
 
@@ -75,18 +301,17 @@ export const Disabled: Story = {
 	args: {
 		label: 'Framework',
 		disabled: true,
-		items
-	}
-};
+		placeholder: 'Select Framwork',
+		'data-testId': 'single-select-disabled'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { label = '', 'data-testId': testId = '' } = args;
+		const container = canvas.getByTestId(testId);
 
-/**
- * Clearable single select.
- */
-export const Clearable: Story = {
-	args: {
-		label: 'Framework',
-		clearable: true,
-		items
+		await step('Check if trigger is disabled', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeDisabled();
+		});
 	}
 };
 
@@ -94,6 +319,59 @@ export const Required: Story = {
 	args: {
 		label: 'Framework',
 		required: true,
-		items
+		'data-testId': 'single-select-required'
+	},
+	play: async ({ canvas, args, step }) => {
+		const { label = '', 'data-testId': testId = '' } = args;
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if required indicator is visible', async () => {
+			const requiredSymbol = within(container).getByText('*');
+			expect(requiredSymbol).toBeVisible();
+		});
+
+		await step('Check if trigger is required', async () => {
+			const trigger = within(container).getByRole('combobox', { name: label });
+			expect(trigger).toBeRequired();
+		});
+	}
+};
+
+export const Status: Story = {
+	render: (args) => {
+		return (
+			<div className="flex flex-col gap-2">
+				<SingleSelect
+					{...args}
+					status="error"
+					label="Error"
+					supportingText="Please select a item."
+				/>
+				<SingleSelect
+					{...args}
+					status="success"
+					label="Success"
+					supportingText="Please select a item."
+				/>
+				<SingleSelect
+					{...args}
+					status="warning"
+					label="Warning"
+					supportingText="Please select a item."
+				/>
+			</div>
+		);
+	}
+};
+
+export const Size: Story = {
+	args: {},
+	render: (args) => {
+		return (
+			<div className="flex flex-col gap-2">
+				<SingleSelect {...args} size="small" label="Small" />
+				<SingleSelect {...args} size="medium" label="Medium" />
+			</div>
+		);
 	}
 };
