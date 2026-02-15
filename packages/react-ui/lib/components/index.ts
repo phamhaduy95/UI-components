f21e44914b68e21ce8d6b08ac99d1a922b8c6fb5
duyph <ph.duy113@gmail.com>
2026-02-16 00:16:23 +0700

    [package][react-ui][feat] add Checkbox

--- packages/react-ui/lib/components/index.ts
+++ packages/react-ui/lib/components/index.ts
@@ -1,41 +1,50 @@
 export * from './Button';
 export { default as Button } from './Button';
 
-export * from './IconButton';
-export { default as IconButton } from './IconButton';
-
 export * from './Calendar';
 export { default as Calendar } from './Calendar';
+
+export * from './Checkbox';
+export { default as Checkbox } from './Checkbox';
 
 export * from './Collapsible';
 export { default as Collapsible } from './Collapsible';
 
-export * from './MultipleCombobox';
-export { default as MultipleCombobox } from './MultipleCombobox';
-
-export * from './SingleCombobox';
-export { default as SingleCombobox } from './SingleCombobox';
-
 export * from './DatePicker';
-
 export { default as DatePicker } from './DatePicker';
-export * from './DateRangePicker';
 
+export * from './DateRangePicker';
 export { default as DateRangePicker } from './DateRangePicker';
-export * from './FieldLabel';
-export { default as FieldLabel } from './FieldLabel';
 
 export * from './DropDownMenu';
 export { default as DropDownMenu } from './DropDownMenu';
 
-export * from './ScrollArea';
-export { default as ScrollArea } from './ScrollArea';
+export * from './FieldLabel';
+export { default as FieldLabel } from './FieldLabel';
 
-export * from './SingleSelect';
-export { default as SingleSelect } from './SingleSelect';
+export * from './IconButton';
+export { default as IconButton } from './IconButton';
+
+export * from './MultipleCombobox';
+export { default as MultipleCombobox } from './MultipleCombobox';
 
 export * from './MultipleSelect';
 export { default as MultipleSelect } from './MultipleSelect';
+
+export * from './NumberInput';
+export { default as NumberInput } from './NumberInput';
+
+export * from './PasswordInput';
+export { default as PasswordInput } from './PasswordInput';
+
+export * from './ScrollArea';
+export { default as ScrollArea } from './ScrollArea';
+
+export * from './SingleCombobox';
+export { default as SingleCombobox } from './SingleCombobox';
+
+export * from './SingleSelect';
+export { default as SingleSelect } from './SingleSelect';
 
 export * from './SupportingText';
 export { default as SupportingText } from './SupportingText';
@@ -45,9 +54,3 @@ export { default as Tag } from './Tag';
 
 export * from './TextInput';
 export { default as TextInput } from './TextInput';
-
-export * from './NumberInput';
-export { default as NumberInput } from './NumberInput';
-
-export * from './PasswordInput';
-export { default as PasswordInput } from './PasswordInput';
