53bf25c4b31bc79b11b0dcc47e3317228426f4ed
phamhaduy95 <68804927+phamhaduy95@users.noreply.github.com>
2026-02-24 18:33:39 +0700

    [package][vue-ui][feat] init vue-ui library (#36)

---
+++ packages/vue-ui/.storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from '@storybook/vue3-vite';
+import '@packages/styles/themes/default.css';
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
+};
+
+export default preview;
---
+++ packages/vue-ui/.storybook/vite-storybook.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [vue(), vueJsx()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, '../lib'),
+			'@components': path.resolve(__dirname, '../lib/components')
+		}
+	}
+});
