999df797aca1040f37baaccffa96ccc40c7390ed
duyph <ph.duy113@gmail.com>
2026-02-25 01:02:16 +0700

    [configs][eslint-config][feat] add eslint config storage

---
+++ configs/eslint-config/base.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+	{
+		ignores: ['dist', 'node_modules', '.next', '.turbo', '.git', '.idea', '.vscode']
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: {
+			js
+		},
+		extends: ['js/recommended']
+	},
+	...tseslint.configs.recommended
+]);
