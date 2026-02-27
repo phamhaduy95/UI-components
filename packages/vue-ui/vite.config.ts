bcf087b56c57520ace37c9887ea3c4a1ea1ae8b7
duyph <ph.duy113@gmail.com>
2026-02-27 07:43:14 +0700

    [package][vue-ui][core] implement build command

--- packages/vue-ui/vite.config.ts
+++ packages/vue-ui/vite.config.ts
@@ -1,17 +1,52 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
+import { globbySync } from 'globby';
+
 export default defineConfig({
-	plugins: [vue(), vueJsx(), vueDevTools()],
+	plugins: [
+		vue(),
+		libInjectCss(),
+		dts({
+			entryRoot: 'lib',
+			staticImport: true,
+			tsconfigPath: './tsconfig.lib.json'
+		})
+	],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
 			'@components': fileURLToPath(new URL('./lib/components', import.meta.url))
 		}
+	},
+	build: {
+		target: 'esnext',
+		lib: {
+			entry: globbySync(['lib/**/index.ts'])
+		},
+		minify: true,
+
+		rollupOptions: {
+			external: ['vue'],
+			output: [
+				{
+					format: 'es',
+					entryFileNames: '[name].js',
+					preserveModules: true,
+					exports: 'named',
+					preserveModulesRoot: 'lib'
+				},
+				{
+					format: 'cjs',
+					preserveModules: true,
+					preserveModulesRoot: 'lib',
+					exports: 'named',
+					entryFileNames: '[name].cjs'
+				}
+			]
+		},
+		copyPublicDir: false
 	}
 });
