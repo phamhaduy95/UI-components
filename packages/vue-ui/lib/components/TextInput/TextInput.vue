979299a8007e5354b509f5771b258a7dde55a6cc
duyph <ph.duy113@gmail.com>
2026-02-28 15:39:48 +0700

    [package][vue-ui][feat] add `TextInput`

---
+++ packages/vue-ui/lib/components/TextInput/TextInput.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+	import { ref, computed, useId } from 'vue';
+	import type { CommonFieldProps } from '@components/type';
+	import { BaseField } from '@components/BaseField';
+	import { IconButton } from '@components/IconButton';
+	import '@packages/styles/components/TextInput.css';
+
+	defineOptions({ inheritAttrs: false });
+
+	export interface TextInputProps extends CommonFieldProps<string> {
+		modelValue?: string;
+		'data-testid'?: string;
+	}
+
+	const props = withDefaults(defineProps<TextInputProps>(), {
+		size: 'medium',
+		required: false,
+		disabled: false,
+		clearable: false
+	});
+
+	const emit = defineEmits<{
+		(e: 'update:modelValue', value: string): void;
+		(e: 'valueChange', value: string): void;
+	}>();
+
+	const inputId = useId();
+	const supportingTextId = useId();
+
+	const internalValue = ref(props.defaultValue || '');
+
+	const value = computed({
+		get: () => (props.modelValue ?? props.value ?? internalValue.value) as string,
+		set: (val: string) => {
+			internalValue.value = val;
+			emit('update:modelValue', val);
+			emit('valueChange', val);
+		}
+	});
+
+	const handleInputChanged = (e: Event) => {
+		const target = e.target as HTMLInputElement;
+		value.value = target.value;
+	};
+
+	const handleClear = () => {
+		value.value = '';
+	};
+
+	const shouldShowClearIcon = computed(
+		() => props.clearable && !!value.value && String(value.value).length > 0
+	);
+</script>
+
+<template>
+	<BaseField
+		class="TextInput"
+		:label="label"
+		:supporting-text="supportingText"
+		:status="status"
+		:required="required"
+		:input-id="inputId"
+		:disabled="disabled"
+		:supporting-text-id="supportingText ? supportingTextId : undefined"
+		:size="size"
+		:data-testId="$attrs['data-testId']"
+	>
+		<div
+			class="BaseField_Field"
+			:data-clearable="clearable"
+			:aria-disabled="disabled"
+		>
+			<input
+				:id="inputId"
+				class="TextInput_Input"
+				:placeholder="placeholder"
+				:disabled="disabled"
+				:aria-disabled="disabled"
+				:aria-describedby="supportingText ? supportingTextId : undefined"
+				:aria-invalid="status === 'error'"
+				:value="value"
+				:required="required"
+				v-bind="$attrs"
+				@input="handleInputChanged"
+			/>
+			<div class="BaseField_Trailing">
+				<IconButton
+					v-if="shouldShowClearIcon"
+					aria-label="Clear"
+					size="medium"
+					variant="text"
+					color="secondary"
+					@click="handleClear"
+				>
+					<svg
+						width="15"
+						height="15"
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+							fill="currentColor"
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</IconButton>
+			</div>
+		</div>
+	</BaseField>
+</template>
---
+++ packages/vue-ui/stories/TextInput.stories.tsx
@@ -0,0 +1,361 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite';
+import { TextInput } from '@components/TextInput';
+import { ref } from 'vue';
+import { expect, within, userEvent, fn } from 'storybook/test';
+
+const mockedOnValueChange = fn();
+
+const meta = {
+	title: 'Components/FormField/TextInput',
+	component: TextInput,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'padded'
+	},
+	argTypes: {
+		onValueChange: { action: 'valueChange' },
+		'onUpdate:modelValue': { action: 'update:modelValue' },
+		status: {
+			control: 'select',
+			options: ['default', 'error', 'success', 'warning']
+		},
+		size: {
+			control: 'select',
+			options: ['small', 'medium', 'large']
+		}
+	},
+	beforeEach() {
+		mockedOnValueChange.mockClear();
+	}
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		label: 'Email',
+		placeholder: 'Enter your email',
+		supportingText: 'Please enter your email address.',
+		'data-testid': 'text-input'
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			return { args };
+		},
+		template: '<TextInput v-bind="args" />'
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { 'data-testid': testId = '', label = '' } = args;
+
+		const container = canvas.getByTestId(testId);
+		await step('Check if container exists', async () => {
+			expect(container).toBeInTheDocument();
+		});
+
+		await step('Check if input exists', async () => {
+			const input = canvas.getByLabelText(label);
+			expect(input).toBeInTheDocument();
+		});
+
+		await step('Check if input has placeholder', async () => {
+			const input = canvas.getByLabelText(label);
+			expect(input).toHaveAttribute('placeholder', args.placeholder);
+		});
+
+		await step('Check if input has supporting text', async () => {
+			const input = canvas.getByLabelText(label);
+			expect(input).toHaveAttribute('aria-describedby');
+
+			const supportingTextId = input.getAttribute('aria-describedby');
+
+			const supportingTextEl = container.querySelector(`#${supportingTextId}`);
+			expect(supportingTextEl).toBeInTheDocument();
+		});
+	}
+};
+
+export const WithDefaultValue: Story = {
+	args: {
+		label: 'Username',
+		defaultValue: 'john doe',
+		clearable: true
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			return { args };
+		},
+		template: '<TextInput v-bind="args" />'
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { label = '', defaultValue = '' } = args;
+
+		let input: HTMLElement;
+		await step('Check if input exists', async () => {
+			input = canvas.getByLabelText(label);
+			expect(input).toBeInTheDocument();
+		});
+
+		await step('Check if input has default value', async () => {
+			expect(input).toHaveValue(defaultValue);
+		});
+
+		await step('Check if clear button is displayed when there is default value', async () => {
+			const clearButton = canvas.getByRole('button', { name: 'Clear' });
+			expect(clearButton).toBeInTheDocument();
+		});
+	}
+};
+
+export const BlankInput: Story = {
+	args: {
+		'data-testid': 'blank-input'
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			return { args };
+		},
+		template: '<TextInput v-bind="args" />'
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { 'data-testid': testId = '' } = args;
+
+		const container = canvas.getByTestId(testId);
+
+		await step('Check if input is blank', async () => {
+			const input = within(container).getByRole('textbox');
+			expect(input).toHaveValue('');
+		});
+
+		await step('Check if input does not have label', async () => {
+			const label = container.querySelector('label');
+			expect(label).not.toBeInTheDocument();
+		});
+
+		await step('Check if input does not have supporting text', async () => {
+			const input = within(container).getByRole('textbox');
+			expect(input).not.toHaveAttribute('aria-describedby');
+		});
+	}
+};
+
+export const Clearable: Story = {
+	args: {
+		label: 'Search',
+		clearable: true,
+		'data-testid': 'clearable-text-input'
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			return { args };
+		},
+		template: '<TextInput v-bind="args" />'
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { label = '', 'data-testid': testId = '' } = args;
+
+		const container = canvas.getByTestId(testId);
+		const input = within(container).getByRole('textbox', { name: label });
+
+		await step('Check if input is not clearable when there is no value', async () => {
+			const clearButton = within(container).queryByRole('button', { name: 'Clear' });
+			expect(clearButton).not.toBeInTheDocument();
+		});
+
+		await step('Check if input is clearable when there is value', async () => {
+			await userEvent.type(input, 'john doe');
+
+			const clearButton = within(container).getByRole('button', { name: 'Clear' });
+			expect(clearButton).toBeInTheDocument();
+		});
+
+		await step('Click on Clear button', async () => {
+			const clearButton = within(container).getByRole('button', { name: 'Clear' });
+			await userEvent.click(clearButton);
+		});
+
+		await step('Check if input is cleared when clear button is clicked', async () => {
+			expect(input).toHaveValue('');
+		});
+
+		await step('Check if clear Button is not displayed', async () => {
+			const clearButton = within(container).queryByRole('button', { name: 'Clear' });
+			expect(clearButton).not.toBeInTheDocument();
+		});
+	}
+};
+
+export const Controllable: Story = {
+	args: {
+		label: 'Username',
+		modelValue: 'initial value',
+		onValueChange: mockedOnValueChange,
+		clearable: true
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			const value = ref(args.modelValue);
+			const handleValueChange = (v: string) => {
+				if (args.onValueChange) args.onValueChange(v);
+				value.value = v;
+			};
+			return () => (
+				<div>
+					<TextInput
+						{...args}
+						modelValue={value.value}
+						onUpdate:modelValue={(val: string) => {
+							value.value = val;
+						}}
+						onValueChange={handleValueChange}
+					/>
+					<p style="margin-left: 8px; margin-top: 12px;" aria-label="Displayed value">
+						Value: {value.value}
+					</p>
+				</div>
+			);
+		}
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { label = '' } = args;
+
+		const input = canvas.getByLabelText(label);
+		await step('Check if input displays initial value', async () => {
+			expect(input).toHaveValue('initial value');
+		});
+
+		await step('Type in value into TextInput', async () => {
+			await userEvent.type(input, ' new value');
+		});
+
+		await step('Check if onValueChange is called with correct arguments', async () => {
+			expect(mockedOnValueChange).toBeCalled();
+			expect(mockedOnValueChange.mock.lastCall).toEqual(['initial value new value']);
+
+			const displayedValue = canvas.getByLabelText('Displayed value');
+			expect(displayedValue).toHaveTextContent('Value: initial value new value');
+		});
+
+		await step('Check if clear button is displayed', async () => {
+			const clearButton = canvas.getByRole('button', { name: 'Clear' });
+			expect(clearButton).toBeInTheDocument();
+		});
+
+		await step('Click in Clear button', async () => {
+			const clearButton = canvas.getByRole('button', { name: 'Clear' });
+			await userEvent.click(clearButton);
+		});
+
+		await step('Check if value should be empty string when user clicks clear button', async () => {
+			expect(mockedOnValueChange).toBeCalled();
+			expect(mockedOnValueChange.mock.lastCall).toEqual(['']);
+
+			const displayedValue = canvas.getByLabelText('Displayed value');
+			expect(displayedValue).toHaveTextContent('Value:');
+		});
+	}
+};
+
+export const Disabled: Story = {
+	args: {
+		label: 'Disabled Input',
+		disabled: true,
+		defaultValue: 'john doe'
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			return { args };
+		},
+		template: '<TextInput v-bind="args" />'
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { label = '' } = args;
+
+		await step('Check if input is disabled', async () => {
+			const input = canvas.getByLabelText(label);
+			expect(input).toBeDisabled();
+		});
+	}
+};
+
+export const Required: Story = {
+	args: {
+		label: 'Required Field',
+		required: true,
+		placeholder: 'This field is required',
+		supportingText: 'This field is required'
+	},
+	render: (args) => ({
+		components: { TextInput },
+		setup() {
+			return { args };
+		},
+		template: '<TextInput v-bind="args" />'
+	}),
+	play: async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement);
+		const { label = '' } = args;
+
+		await step('Check if input is required', async () => {
+			const input = canvas.getByLabelText(label);
+			expect(input).toBeRequired();
+		});
+	}
+};
+
+export const Sizes: Story = {
+	render: () => ({
+		components: { TextInput },
+		setup() {
+			return () => (
+				<div style="display: flex; flex-direction: column; gap: 16px;">
+					<TextInput label="Small" size="small" placeholder="Enter your email" />
+					<TextInput label="Medium" size="medium" placeholder="Enter your email" />
+				</div>
+			);
+		}
+	})
+};
+
+export const Statuses: Story = {
+	render: () => ({
+		components: { TextInput },
+		setup() {
+			return () => (
+				<div style="display: flex; flex-direction: column; gap: 16px;">
+					<TextInput
+						label="Error"
+						placeholder="Enter your email"
+						supportingText="Please enter your email address."
+						status="error"
+					/>
+					<TextInput
+						label="Warning"
+						placeholder="Enter your email"
+						supportingText="Please enter your email address."
+						status="warning"
+					/>
+					<TextInput
+						label="Success"
+						placeholder="Enter your email"
+						supportingText="Please enter your email address."
+						status="success"
+					/>
+				</div>
+			);
+		}
+	})
+};
