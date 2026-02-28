f20a65a61a5d78fe07444d69b927e7713a2c4c00
phamhaduy95 <68804927+phamhaduy95@users.noreply.github.com>
2026-02-28 16:55:30 +0700

    [package][react-ui][core] add lightningcss (#49)

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,36 +1,26 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import * as glob from 'glob';
 import path, { extname, relative } from 'path';
-import { fileURLToPath } from 'url';
+import { globbySync } from 'globby';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { fileURLToPath } from 'url';
 
-const components: Array<[string, string]> = glob
-	.sync('lib/**/*.{ts,tsx}', {
-		ignore: ['lib/**/*.d.ts', 'lib/**/type.ts']
-	})
-	.map((file) => [
-		relative('lib', file.slice(0, file.length - extname(file).length)),
-		fileURLToPath(new URL(file, import.meta.url))
-	]);
-
-const themes = glob
-	.sync('lib/themes/**/*.css')
-	.map((file) => [
-		relative('lib/themes', file.slice(0, file.length - extname(file).length)),
-		fileURLToPath(new URL(file, import.meta.url))
-	]);
+const components: Array<[string, string]> = globbySync('lib/**/*.{ts,tsx}', {
+	ignore: ['lib/**/*.d.ts', 'lib/**/type.ts']
+}).map((file) => [
+	relative('lib', file.slice(0, file.length - extname(file).length)),
+	fileURLToPath(new URL(file, import.meta.url))
+]);
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		tailwindcss(),
 		libInjectCss(),
 		dts({
 			entryRoot: 'lib',
+			staticImport: true,
 			tsconfigPath: './tsconfig.lib.json'
 		})
 	],
@@ -39,14 +29,17 @@ export default defineConfig({
 			'@components': path.resolve(__dirname, './lib/components')
 		}
 	},
+	css: {
+		transformer: 'lightningcss'
+	},
 	build: {
 		target: 'esnext',
 		lib: {
-			entry: Object.fromEntries([...components, ...themes])
+			entry: Object.fromEntries(components)
 		},
-		sourcemap: true,
 		rollupOptions: {
 			external: ['react', 'react-dom', 'react/jsx-runtime'],
+
 			output: [
 				{
 					format: 'cjs',
@@ -63,6 +56,8 @@ export default defineConfig({
 				},
 				{
 					format: 'es',
+					chunkFileNames: 'common/[name].js',
+					entryFileNames: '[name].js',
 					assetFileNames: (a) => {
 						if (a.originalFileNames.length > 0) {
 							const targetDir = relative('lib', path.dirname(a.originalFileNames[0]));
@@ -70,9 +65,7 @@ export default defineConfig({
 							return fileName;
 						}
 						return 'assets/[name][extname]';
-					},
-					entryFileNames: '[name].js',
-					chunkFileNames: 'common/[name].js'
+					}
 				}
 			]
 		},
