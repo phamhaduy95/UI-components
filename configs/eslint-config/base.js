c2ca2645b7acf90f4431dd1ed117facec3a1ea80
phamhaduy95 <68804927+phamhaduy95@users.noreply.github.com>
2026-03-08 16:28:52 +0700

    [configs][eslint][feat] add import rule (#76)

--- configs/eslint-config/base.js
+++ configs/eslint-config/base.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { configs as tsConfig } from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
@@ -13,7 +13,7 @@ export default defineConfig([
 		},
 		extends: [js.configs.recommended]
 	},
-	...tseslint.configs.recommended,
+	...tsConfig.recommended,
 	{
 		rules: {
 			'no-console': 'error',
