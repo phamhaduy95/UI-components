a84032ec09245ef473edb48d6b20954cce2e5bc4
duyph <ph.duy113@gmail.com>
2026-04-04 22:52:23 +0700

    [package][vue-ui][refactor] refactor code structure for `BaseCombobox`

--- packages/vue-ui/lib/components/BaseCombobox/index.ts
+++ packages/vue-ui/lib/components/BaseCombobox/index.ts
@@ -1,2 +1,2 @@
 export { default as BaseCombobox } from './BaseCombobox.vue';
-export type { BaseComboboxProps, BaseComboboxEmits } from './BaseCombobox.type';
+export type { BaseComboboxProps, BaseComboboxEmits, BaseComboboxSlots } from './BaseCombobox.type';
