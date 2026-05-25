2c30eb44c5b0cded8d014af9334e5c63c96c44c7
phamhaduy95 <68804927+phamhaduy95@users.noreply.github.com>
2026-05-25 13:16:20 +0700

    [packages][vue-ui][feat] add `ColorPicker` (#116)

---
+++ packages/vue-ui/lib/components/ColorPicker/ColorPicker.type.ts
@@ -0,0 +1,41 @@
+import type { HTMLAttributes } from 'vue';
+import type { CommonFieldProps } from '@components/type';
+import type {
+	ColorPickerRootEmits,
+	ColorPickerColorFormat as ArkColorFormat,
+	ColorPickerRootBaseProps,
+	Color
+} from '@ark-ui/vue/color-picker';
+
+export type ColorValue = Pick<Color, 'toString' | 'isEqual'>;
+
+export type ColorPickerColorFormat = ArkColorFormat | 'hex';
+
+export interface BaseColorPickerProps
+	extends CommonFieldProps<string>,
+		Pick<
+			ColorPickerRootBaseProps,
+			| 'closeOnSelect'
+			| 'lazyMount'
+			| 'positioning'
+			| 'unmountOnExit'
+			| 'openAutoFocus'
+			| 'open'
+			| 'defaultOpen'
+		> {
+	modelValue?: string;
+	defaultValue?: string;
+	defaultFormat?: ColorPickerColorFormat;
+	format?: ColorPickerColorFormat;
+	dataTestid?: string;
+}
+
+export type ColorPickerProps = BaseColorPickerProps & /*@vue-ignore */ HTMLAttributes;
+
+export type ColorPickerEmits = {
+	'update:modelValue': [value: string];
+	'update:open': ColorPickerRootEmits['update:open'];
+	'update:format': [format: ColorPickerColorFormat];
+	'value-change-end': [value: string, colorValue: ColorValue];
+	'value-change': [value: string, colorValue: ColorValue];
+};
